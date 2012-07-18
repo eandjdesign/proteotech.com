2eff8cc9d0b562cc6c53a09e359ba62e5d83348a
Eric Ritchey <Eric@mojo-home.local>
2012-07-18 16:25:06 -0700

    Adding 7/18/2012 press release

--- js/pageparts/news.js
+++ js/pageparts/news.js
@@ -8,11 +8,11 @@ document.write('<h3>News</h3>\
 		<div id="p7VSC_1" class="p7VSC07">\
 			<div id="p7VSCb_1" class="p7VSC_scrollbox">\
 				<div id="p7VSCd_1_c1" class="p7VSC_scrolling">\
+					<p class="news-item"><span>July 18, 2012</span> ProteoTech to present at The Alzheimer\'s Association International Conference 2012 (AAIC)&reg; in Vancouver, British Columbia, Canada.</p>\
 					<p class="news-item"><span>April 23, 2012</span> ProteoTech\'s Chief Scientific Officer, Dr. Alan Snow, to Present New Data at the XIIIth International Symposium on Amyloidosis</p>\
 					<p class="news-item"><span>December 20, 2011</span> ProteoTech Announces Drug Development Collaborative Agreement with GlaxoSmithKline for New Parkinson\'s Therapeutic</p>\
 					<p class="news-item"><span>September 22, 2011</span> ProteoTech Presented at The 5th Annual Neurodegenerative Conditions Research and Development Conference in San Francisco, California</p>\
 					<p class="news-item"><span>July 16-21, 2011</span> ProteoTech to Present at Alzheimer’s Association International Conference (AAIC) 2011, in Paris, France</p>\
-					<p class="news-item"><span>June 15, 2011</span> ProteoTech Awarded $770,675 U.S. Government Grant from the National Institute of Health</p>\
 					<a href="/news/news_press.html" title="Read More News">Read More</a>\
 				</div>\
 			</div>\
