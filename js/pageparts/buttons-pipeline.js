837a5492eb73bb940d3f450d002b8c1ed3ec1491
Eric Ritchey <eric@eandjdesign.com>
2014-04-25 00:41:13 -0700

    changing all exebryl to pti-80

--- js/pageparts/buttons-pipeline.js
+++ js/pageparts/buttons-pipeline.js
@@ -1,6 +1,6 @@
 document.write('<ul>\
-<li><a href="/pipeline/exebryl.html" title="Exebryl-1&trade;">Exebryl-1&trade;</a></li>\
-<li><a href="/pipeline/pepticlere.html" title="Pepticlere&trade;">Pepticlere&trade;</a></li>\
-<li><a href="/pipeline/synuclere.html" title="Synuclere&trade;">Synuclere&trade;</a></li>\
-<li><a href="/pipeline/systebryl.html" title="Systebryl&trade;">Systebryl&trade;</a></li>\
+<li><a href="/pipeline/pti-80.html" title="PTI-80">PTI-80</a></li>\
+<li><a href="/pipeline/pepticlere.html" title="Pepticlere&trade; (DP-74)">Pepticlere&trade; (DP-74)</a></li>\
+<li><a href="/pipeline/synuclere.html" title="Synuclere&trade; (PD-61-W3)">Synuclere&trade; (PD-61-W3)</a></li>\
+<li><a href="/pipeline/systebryl.html" title="Systebryl&trade; (PTI-110)">Systebryl&trade; (PTI-110)</a></li>\
 </ul>');
