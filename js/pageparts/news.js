56b0d74fcc463eb372a63016ca8c665b658f3c03
Eric Ritchey <eric@eandjdesign.com>
2014-04-25 01:49:49 -0700

    news

--- js/pageparts/news.js
+++ js/pageparts/news.js
@@ -8,13 +8,14 @@ document.write('<h3>News</h3>\
 		<div id="p7VSC_1" class="p7VSC07">\
 			<div id="p7VSCb_1" class="p7VSC_scrollbox">\
 				<div id="p7VSCd_1_c1" class="p7VSC_scrolling">\
-					<p class="news-item"><span>April, 2014</span> Dr. Joann Data Joins ProteoTech as Chief Medical Officer.</p>\
-					<p class="news-item"><span>January, 2014</span> Dr. Roger Flugel Joins ProteoTech as Chief Business Officer.</p>\
+					<p class="news-item"><span>May 7, 2014</span> ProteoTech Invited to Speak at the Prestigious Cavendish Global Health Impact Forum at the United Nations, New York, NY</p>\
+					<p class="news-item"><span>May 1, 2014</span> ProteoTech’s Chief Scientific Officer to Present at the XIV International Symposium on Amyloidosis, Indianapolis, IN</p>\
+					<p class="news-item"><span>April 8, 2014</span> ProteoTech’s New Chief Business Officer (Dr. Roger Flugel) Presents at the 13th Annual Needham Healthcare Conference, New York, NY</p>\
+					<p class="news-item"><span>March 21, 2014</span> Dr. Joann Data Joins ProteoTech as Chief Medical Officer.</p>\
+					<p class="news-item"><span>February 15, 2014</span> Dr. Roger Flugel Joins ProteoTech as Chief Business Officer.</p>\
+					<p class="news-item"><span>September 1, 2013</span> ProteoTech Receives $2.29M Phase II SBIR Award from the National Institute on Aging to Identify Small Molecule Inhibitors of Tau Protein Aggregation for Alzheimer\'s Disease and other Taupathies.</p>\
+					<p class="news-item"><span>August 1, 2013</span> Dr. Rudolph Tanzi Joins ProteoTech’s Scientific Advisory Board</p>\
 					<p class="news-item"><span>May, 2013</span> ProteoTech to Receive $2.27M Phase II SBIR Award from the National Institute on Aging to Identify Small Molecule Inhibitors of Tau Protein Aggregation for Alzheimer\'s Disease and other Taupathies.</p>\
-					<p class="news-item"><span>July 18, 2012</span> ProteoTech to present at The Alzheimer\'s Association International Conference 2012 (AAIC)&reg; in Vancouver, British Columbia, Canada.</p>\
-					<p class="news-item"><span>April 23, 2012</span> ProteoTech\'s Chief Scientific Officer, Dr. Alan Snow, to Present New Data at the XIIIth International Symposium on Amyloidosis</p>\
-					<p class="news-item"><span>September 22, 2011</span> ProteoTech Presented at The 5th Annual Neurodegenerative Conditions Research and Development Conference in San Francisco, California</p>\
-					<p class="news-item"><span>July 16-21, 2011</span> ProteoTech to Present at Alzheimer’s Association International Conference (AAIC) 2011, in Paris, France</p>\
 					<a href="/news/news_press.html" title="Read More News">Read More</a>\
 				</div>\
 			</div>\
