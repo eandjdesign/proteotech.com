848aa9389fdd251f6f65ecf6c183ebb119ed2840
Eric Ritchey <eritchey@teslamotors.com>
2015-04-28 23:07:14 -0700

    adding 2015-04-28 cognitive clarity postings

--- js/pageparts/news.js
+++ js/pageparts/news.js
@@ -3,24 +3,25 @@
  * this is the best/most flexible thing given the server's architecture
  */
 document.write('<h3>News</h3>\
-	<div class="aside">\
-		<div id="p7VSC_1" class="p7VSC07">\
-			<div id="p7VSCb_1" class="p7VSC_scrollbox">\
-				<div id="p7VSCd_1_c1" class="p7VSC_scrolling">\
-					<p class="news-item"><span>March 18–22, 2015</span> ProteoTech Presents Updates on Synuclere&trade; and TauPro&trade; at The 12th International Conference on Alzheimer\'s and Parkinson\'s Diseases</p>\
-					<p class="news-item"><span>January 12, 2015</span> ProteoTech is Granted Orphan Drug Designation for Systebryl&trade; (PTI-110) for the Treatment of AL  Amyloidosis</p>\
-					<p class="news-item"><span>January 12, 2015</span> ProteoTech Appoints Dr. Roger Flugel as Chief Executive Officer</p>\
-					<p class="news-item"><span>July 16, 2014</span> Dr. Qubai Hu, Director of Molecular Biology, to present at the Alzheimer\'s Association International Conference (AAIC) &reg;</p>\
-					<p class="news-item"><span>May 5, 2014</span> ProteoTech Selected to Present at the Prestigious Cavendish Global Health Impact Forum at the United Nations, New York, NY</p>\
-					<p class="news-item"><span>May 1, 2014</span> ProteoTech’s Chief Scientific Officer to Present at the XIV International Symposium on Amyloidosis, Indianapolis, IN</p>\
-					<p class="news-item"><span>April 8, 2014</span> ProteoTech’s New Chief Business Officer (Dr. Roger Flugel) Presents at the 13th Annual Needham Healthcare Conference, New York, NY</p>\
-					<p class="news-item"><span>March 21, 2014</span> Dr. Joann Data Joins ProteoTech as Chief Medical Officer.</p>\
-					<p class="news-item"><span>February 15, 2014</span> Dr. Roger Flugel Joins ProteoTech as Chief Business Officer.</p>\
-					<p class="news-item"><span>September 1, 2013</span> ProteoTech Receives $2.29M Phase II SBIR Award from the National Institute on Aging to Identify Small Molecule Inhibitors of Tau Protein Aggregation for Alzheimer\'s Disease and other Taupathies.</p>\
-					<a href="/news/news_press.html" title="Read More News">Read More</a>\
-				</div>\
-			</div>\
-			<script type="text/javascript">P7_opVSC(\'p7VSCb_1\',1,25,2,10,10,250,1,1,1,1,1200,300,2000,1);</script>\
-		</div>\
-	</div>\
+    <div class="aside">\
+        <div id="p7VSC_1" class="p7VSC07">\
+            <div id="p7VSCb_1" class="p7VSC_scrollbox">\
+                <div id="p7VSCd_1_c1" class="p7VSC_scrolling">\
+                    <p class="news-item"><span>March 18–22, 2015</span> ProteoTech Presents Update on its New Product "Cognitive Clarity&trade;" at The 12th International Conference on Alzheimer\'s and Parkinson\'s Diseases</p>\
+                    <p class="news-item"><span>March 18–22, 2015</span> ProteoTech Presents Updates on Synuclere&trade; and TauPro&trade; at The 12th International Conference on Alzheimer\'s and Parkinson\'s Diseases</p>\
+                    <p class="news-item"><span>January 12, 2015</span> ProteoTech is Granted Orphan Drug Designation for Systebryl&trade; (PTI-110) for the Treatment of AL  Amyloidosis</p>\
+                    <p class="news-item"><span>January 12, 2015</span> ProteoTech Appoints Dr. Roger Flugel as Chief Executive Officer</p>\
+                    <p class="news-item"><span>July 16, 2014</span> Dr. Qubai Hu, Director of Molecular Biology, to present at the Alzheimer\'s Association International Conference (AAIC) &reg;</p>\
+                    <p class="news-item"><span>May 5, 2014</span> ProteoTech Selected to Present at the Prestigious Cavendish Global Health Impact Forum at the United Nations, New York, NY</p>\
+                    <p class="news-item"><span>May 1, 2014</span> ProteoTech’s Chief Scientific Officer to Present at the XIV International Symposium on Amyloidosis, Indianapolis, IN</p>\
+                    <p class="news-item"><span>April 8, 2014</span> ProteoTech’s New Chief Business Officer (Dr. Roger Flugel) Presents at the 13th Annual Needham Healthcare Conference, New York, NY</p>\
+                    <p class="news-item"><span>March 21, 2014</span> Dr. Joann Data Joins ProteoTech as Chief Medical Officer.</p>\
+                    <p class="news-item"><span>February 15, 2014</span> Dr. Roger Flugel Joins ProteoTech as Chief Business Officer.</p>\
+                    <p class="news-item"><span>September 1, 2013</span> ProteoTech Receives $2.29M Phase II SBIR Award from the National Institute on Aging to Identify Small Molecule Inhibitors of Tau Protein Aggregation for Alzheimer\'s Disease and other Taupathies.</p>\
+                    <a href="/news/news_press.html" title="Read More News">Read More</a>\
+                </div>\
+            </div>\
+            <script type="text/javascript">P7_opVSC(\'p7VSCb_1\',1,25,2,10,10,250,1,1,1,1,1200,300,2000,1);</script>\
+        </div>\
+    </div>\
 </div>');
